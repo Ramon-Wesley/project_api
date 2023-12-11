32e7adaecd3d1ba8e7a25ac7310b10cf44b58023
Ramon Silva <ramonwj.s@outlook.com>
2023-12-11 13:25:54 -0300

    config infra db sequelize products

---
+++ src/infrastructure/db/sequelize/checkout/products/repository/ProductRepository.spec.ts
@@ -0,0 +1,101 @@
+import { Op } from "sequelize";
+import Product from "../../../../../../domain/checkout/products/entity/Product";
+import ProductFactory from "../../../../../../domain/checkout/products/factory/Product.factory";
+import SequelizeFactory from "../../../factory/Sequelize.factory";
+import ProductModel from "../model/ProductModel";
+import ProductRepository from "./ProductRepository";
+import { Sequelize } from "sequelize-typescript";
+
+
+describe("Test the product repository", ()=>{
+  
+   
+    let sequelize:Sequelize;  
+
+      
+    beforeEach(async()=>{ 
+        sequelize= await SequelizeFactory.execute();          
+  
+      })
+  
+       afterEach(async()=>{
+          await sequelize.close()
+          
+      })
+    it("save product data correctly",async()=>{
+        const productRepository= new ProductRepository();
+        const product=ProductFactory.create("product1",10)
+        await productRepository.create(product);
+        const findProduct=await ProductModel.findByPk(product.Id);
+
+        expect(findProduct?.toJSON()).toStrictEqual({
+            id:product.Id,
+            name:product.Name,
+            price:product.Price
+        })
+        
+    })
+
+    it("update product data correctly",async()=>{
+        const productRepository= new ProductRepository();
+        const product=ProductFactory.create("product1",10)
+        await productRepository.create(product);
+        const findProduct=await ProductModel.findByPk(product.Id);
+
+        expect(findProduct?.toJSON()).toStrictEqual({
+            id:product.Id,
+            name:product.Name,
+            price:product.Price
+        })
+        const productUpdate=new Product(product.Id,"product2",12);
+        await productRepository.updateById(product.Id,productUpdate);
+        const findProductUpdate=await ProductModel.findByPk(product.Id);
+
+        expect(findProductUpdate?.toJSON()).toStrictEqual({
+            id:product.Id,
+            name:productUpdate.Name,
+            price:productUpdate.Price
+        })
+
+        
+    })
+
+   it("find product data by id",async()=>{
+    const productRepository= new ProductRepository();
+        const product=ProductFactory.create("product1",10)
+        await productRepository.create(product);
+        const findProduct=await ProductModel.findByPk(product.Id);
+        const findResult=await productRepository.findById(product.Id)
+        expect(findProduct?.toJSON()).toStrictEqual({
+            id:findResult.Id,
+            name:findResult.Name,
+            price:findResult.Price
+        })
+   })
+
+    it("find all product data",async ()=>{
+        const productRepository= new ProductRepository();
+        const product=ProductFactory.create("product1",10)
+        const product2= ProductFactory.create("product2",12);
+        await productRepository.create(product);
+        await productRepository.create(product2);
+        
+        const sort="desc"
+        const filter=""
+        const limit=7
+        const page=1
+
+        const findAllProduct=await productRepository.findAll(sort,filter,limit,page)
+        
+        expect(findAllProduct.entity[0].Id).toBe(product2.Id)
+        expect(findAllProduct.entity[0].Name).toBe(product2.Name)
+        expect(findAllProduct.entity[0].Price).toBe(product2.Price)
+        expect(findAllProduct.entity[1].Id).toBe(product.Id)
+        expect(findAllProduct.entity[1].Name).toBe(product.Name)
+        expect(findAllProduct.entity[1].Price).toBe(product.Price)
+        expect(findAllProduct.current_page).toBe(page)
+        expect(findAllProduct.number_of_elements).toBe(2)
+        expect(findAllProduct.total_page).toBe(1)
+    })
+
+})
---
+++ src/infrastructure/db/sequelize/checkout/products/repository/ProductRepository.ts
@@ -0,0 +1,85 @@
+import { Op } from "sequelize";
+import RepositoryFindAllResult from "../../../../../../domain/@shared/repository/RepositoryFindAllResult";
+import Product from "../../../../../../domain/checkout/products/entity/Product";
+import ProductRepositoryInterface from "../../../../../../domain/checkout/products/repository/ProdoctRepositoryInterface";
+import ProductModel from "../model/ProductModel";
+
+export default class ProductRepositorySequelize implements ProductRepositoryInterface{
+    async create(entity: Product): Promise<void> {
+        try {
+            await ProductModel.create({
+                id:entity.Id,
+                name:entity.Name,
+                price:entity.Price
+            })
+        } catch (error) {
+            throw new Error("error creating product record\n"+error)
+        }
+            
+    }
+    
+    async findById(id: string): Promise<Product> {
+        try {
+            const findResult=await ProductModel.findByPk(id);
+            if(findResult){
+                const result=new Product(findResult.id,findResult.name,findResult.price)
+                return result
+            }
+            throw new Error("product not found!\n")
+        } catch (error) {
+            throw new Error("error find product record\n"+error)
+        }
+       
+        
+    }
+    async findAll(sort: "desc" | "asc", filter: string, limit: number, page: number): Promise<RepositoryFindAllResult<Product>> {
+      try {
+        let findAllProductResult:Product[]=[]
+       
+          const result=await ProductModel.findAndCountAll({
+            where:{name:{[Op.like]:`%${filter}%`}},
+            order:[["name",sort]],
+            limit:limit,
+            offset:(page-1)*limit,
+        
+          })
+
+           const  totalElements=result.count
+           const totalPages = Math.ceil(totalElements / limit);
+            let product:Product;
+            result.rows.forEach((res)=>{
+                product=new Product(res.id,res.name,res.price)
+                findAllProductResult.push(product)
+            })
+
+          
+          const findAllResult:RepositoryFindAllResult<Product>={
+            entity:findAllProductResult,
+            current_page:page,
+            number_of_elements:totalElements,
+            total_page:totalPages
+           };
+           return findAllResult
+      } catch (error) {
+        throw new Error("error listing product record!\n"+error)
+      }
+    }
+    async deleteById(id: string): Promise<void> {
+        try {
+          await ProductModel.destroy({where:{id:id}})
+        } catch (error) {
+            throw new Error("error when deleting product record!\n"+error)
+        }
+    }
+    async updateById(id: string, entity: Product): Promise<void> {
+        try {
+            await ProductModel.update({
+                name:entity.Name,
+                price:entity.Price
+            },{where:{id:id}})       
+        } catch (error) {
+            throw new Error("error update product record\n"+error)
+        }
+}
+
+}
