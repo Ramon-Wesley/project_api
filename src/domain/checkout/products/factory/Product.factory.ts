150c515da45e28b3f4ccc1f9e0859eb0b66960bc
Ramon Silva <ramonwj.s@outlook.com>
2023-12-29 15:26:48 -0300

    Revert "add category in product domain"
    This reverts commit cf7a5e35701061084d527fbc058980141df632bb.

--- src/domain/checkout/products/factory/Product.factory.ts
+++ src/domain/checkout/products/factory/Product.factory.ts
@@ -1,11 +1,10 @@
 import { v4 as uuidv4 } from "uuid";
 import Product from "../entity/Product";
 
-
 export default class ProductFactory{
 
-    static create(name:string,price:number,quantity:number,category_id:string){
+    static create(name:string,price:number){
         const uuid=uuidv4()
-        return new Product(uuid,name,price,quantity,category_id)
+        return new Product(uuid,name,price)
     }
 }
