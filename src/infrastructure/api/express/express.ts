7c6b81b2ea9e0b45d24d6e00a5b822de07efd3be
ramon.silva <ramonwj.s@outlook.com>
2024-09-21 20:41:32 -0300

    add cors import

--- src/infrastructure/api/express/express.ts
+++ src/infrastructure/api/express/express.ts
@@ -2,7 +2,7 @@ import express,{ Express } from "express"
 import DbFactory from "../../db/factory/DbFactory";
 import {router} from "../routes/index"
 import CacheFactory from "../../cache/factory/CacheConnectFactory";
-
+import cors from "cors"
 
 export default class ExpressConfig{
     
@@ -12,6 +12,7 @@ export default class ExpressConfig{
 
      static async execute():Promise<Express>{
         this.app.use(express.json());
+        this.app.use(cors())
         this.app.use(router);
         await DbFactory.execute();
         return this.app
